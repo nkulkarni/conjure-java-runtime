c7d3e42348c1cd006ff566dd02ca2e991cc9afe2
Patrick Koenig <pkoenig10@gmail.com>
2020-05-01 07:35:30 -0700

    Set cause of IoUnknownRemoteException

--- okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/RemotingOkHttpCall.java
+++ okhttp-clients/src/main/java/com/palantir/conjure/java/okhttp/RemotingOkHttpCall.java
@@ -656,6 +656,7 @@ final class RemotingOkHttpCall extends ForwardingCall {
         private final UnknownRemoteException unknownRemoteException;
 
         private IoUnknownRemoteException(UnknownRemoteException unknownRemoteException) {
+            super(unknownRemoteException);
             this.unknownRemoteException = unknownRemoteException;
         }
 
